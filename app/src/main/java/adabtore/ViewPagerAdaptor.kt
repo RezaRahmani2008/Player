b9acb0afeb60d0b8736af2adbbeebd46a0d5423b
Rio <Rioz2008x@gmail0com>
2023-05-24 08:16:45 +0330

    finish tabLayout and ViewPager2

---
+++ app/src/main/java/adabtore/ViewPagerAdaptor.kt
@@ -0,0 +1,17 @@
+package adabtore
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdaptor(var fragmentList:MutableList<Fragment>, fragmentManager: FragmentManager, lifecycle: Lifecycle)
+    : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(Position: Int): Fragment {
+        return fragmentList[Position]
+    }
+}
